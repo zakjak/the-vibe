272ac39a7f8d3c2e4483fdd3781ca2e2456e08ed
Yakubu Zakaria <78931393+zakjak@users.noreply.github.com>
2025-10-26 23:38:02 -0500

    update next-config to incldeu env

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,11 @@ const nextConfig: NextConfig = {
     ],
   },
   eslint: { ignoreDuringBuilds: true },
+  env: {
+    DATABASE_URL_NEON: process.env.DATABASE_URL_NEON,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    AUTH_SECRET: process.env.AUTH_SECRET,
+  },
 };
 
 export default nextConfig;
