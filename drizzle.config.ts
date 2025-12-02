58b8f99af6a972b9b236b410b4db57f14bd90fbb
Yakubu Zakaria <78931393+zakjak@users.noreply.github.com>
2025-12-02 00:55:32 -0600

    changed drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   dialect: "postgresql",
-  schema: "./lib/schema/*",
+  schema: "./lib/schema",
   out: "./drizzle",
   dbCredentials: {
     url: process.env.DATABASE_URL_NEON as string,
