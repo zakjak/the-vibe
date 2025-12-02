ee9a0ce8860a2de05b7e79621a5dca7d55412343
Yakubu Zakaria <78931393+zakjak@users.noreply.github.com>
2025-12-02 03:42:12 -0600

    added utc to then constant in helpers calculateTime

--- lib/utils/helpers.ts
+++ lib/utils/helpers.ts
@@ -8,20 +8,19 @@ export const calculateTime = (date?: string) => {
   const cleanDate = date.split(".")[0]; // "2025-11-19 00:23:51"
 
   const now = moment();
-  const then = moment(cleanDate, "YYYY-MM-DD HH:mm:ss").local(); // parse as local
+  const then = moment.utc(cleanDate, "YYYY-MM-DD HH:mm:ss").local(); // parse as local
 
   const diff = now.diff(then);
   const duration = moment.duration(diff);
 
-  if (duration.asSeconds() < 60) {
+  if (duration.asSeconds() < 60)
     return `${Math.floor(duration.asSeconds())} seconds ago`;
-  } else if (duration.asMinutes() < 60) {
+  if (duration.asMinutes() < 60)
     return `${Math.floor(duration.asMinutes())} minutes ago`;
-  } else if (duration.asHours() < 24) {
+  if (duration.asHours() < 24)
     return `${Math.floor(duration.asHours())} hours ago`;
-  } else if (duration.asDays() < 30) {
+  if (duration.asDays() < 30)
     return `${Math.floor(duration.asDays())} days ago`;
-  }
 
   return then.format("D MMM YYYY");
 };
